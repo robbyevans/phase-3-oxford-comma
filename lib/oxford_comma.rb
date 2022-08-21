5adff8b454d9d351fd1f7547c71ec58e9eb189c1
Evans Rutto <robbyevans001@gmail.com>
2022-08-21 09:59:44 +0300

    commit tree 365f84c3ffcc44dc5aafe96fb81b707da41d7d60

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
+
+array=["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+
+# array=['box',"border_box","sling"]
 def oxford_comma(array)
+  if array.length==1
+    pp array.join("")
+  elsif array.length==2
+    (array.insert(array.length-1," and #{array[array.length-1]}"))
+    array.pop
+    pp array.join("")
+
+  elsif array.length>2
+  new_array=(array.insert(array.length-1,"and #{array[array.length-1]}"))
+  array.pop
+  pp new_array.join(", ")
+  end
 
 end
+oxford_comma(array)
+
+
+# pp oxford_comma(["fiddleheads","okra","kohlrabi"])
+
+
